c435304b77ec4d8174905e794a1a14bb5ce2ff18
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-05-26 16:22:49 +0530

    26-05-2026 all data uploaded

---
+++ module-6-javascript&jquery/calculator-app/js/main.js
@@ -0,0 +1,50 @@
+// create a function for clear values
+function clrValue()
+{
+document.getElementById("result").value="";    
+}
+// slice a values of input create a function 
+function slcData()
+{
+  var slc=document.getElementById("result").value;
+  var res=slc.slice(0,-1);
+  document.getElementById("result").value=res;  
+}
+// create a function a for input a button values 
+function inpData(val)
+{
+ document.getElementById("result").value+=val;     
+}
+// create a function for final Results in calculator 
+function finalResult()
+{ 
+  var x=document.getElementById("result").value;
+  var y=eval(x);
+  document.getElementById("result").value=y;
+}
+
+// calculate a square 
+function Square()
+{
+var sq=document.getElementById("result").value;
+var res=Math.pow(sq,2);
+document.getElementById("result").value=res;
+}
+
+// calculate a squareRoot 
+function SquareRoot()
+{
+var sq=document.getElementById("result").value;
+var res=Math.sqrt(sq);
+document.getElementById("result").value=res;
+
+}
+
+
+// right click disables 
+
+const protectedImage = document.getElementById('my-protected-img');
+
+protectedImage.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+});
